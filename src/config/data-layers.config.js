6bfd1304bfdb1cff1c682f77e467e3589116b2bd
Reid Greer <jreid.greer@gmail.com>
2017-07-08 17:58:22 -0700

    feat: Adds initial source files for build testing

---
+++ src/config/data-layers.config.js
@@ -0,0 +1,22 @@
+export default {
+  layers: [
+    {
+      id: 'gtm',
+      displayName: 'Google Tag Manager (GTM)',
+      type: 'windowProperty',
+      key: 'dataLayer',
+    },
+    {
+      id: 'tealium',
+      displayName: 'Tealium',
+      type: 'windowProperty',
+      key: 'utag_data',
+    },
+    {
+      id: 'dtm',
+      displayName: 'Adobe Dynamic Tag Management',
+      type: 'windowProperty',
+      key: 'utag_data',
+    },
+  ]
+}
