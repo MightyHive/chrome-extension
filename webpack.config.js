c16c50e5a89bc2c34d8092a295678ca1d396d5af
Reid Greer <jreid.greer@gmail.com>
2017-07-08 17:57:58 -0700

    build: Creates the build script from source

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+// *****
+// Based on https://github.com/alicoding/react-webpack-babel
+// *****
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/index.js',
+    './styles/index.scss',
+  ],
+  output: {
+    publicPath: './',
+    path: path.join(__dirname, 'dist'),
+    filename: '[chunkhash].js',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components|public\/)/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader?importLoaders=1'],
+        exclude: ['node_modules'],
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader?sourceMap&localIdentName=[local]___[hash:base64:5]!sass-loader?outputStyle=expanded',
+        }),
+        exclude: ['node_modules'],
+      },
+    ],
+  },
+  plugins: [
+    new WebpackCleanupPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
+      },
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        drop_console: true,
+        drop_debugger: true,
+      },
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new ExtractTextPlugin({
+      filename: 'style.css',
+      allChunks: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/popup.html',
+      files: {
+        css: ['style.css'],
+        js: ['bundle.js'],
+      },
+    }),
+  ],
+};
