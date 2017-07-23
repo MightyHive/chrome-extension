23ec391002f0c700a02049881f003293d7334585
Reid Greer <jreid.greer@gmail.com>
2017-07-22 22:51:18 -0700

    build: Minor updates to improve dev

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,11 @@
 module.exports = {
-    "extends": "airbnb",
-    "rules": {
-    }
+    extends: 'airbnb',
+    rules: {
+      'no-console': 'off'
+    },
+    globals: {
+      window: false,
+      document: false,
+      chrome: false
+  }
 }
