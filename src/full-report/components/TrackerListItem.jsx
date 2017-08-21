59acf853a11f620321d8ea067ebc9a86295c248e
Reid Greer <jreid.greer@gmail.com>
2017-08-20 18:31:34 -0700

    feat: Displays associated network calls with trackers

--- src/full-report/components/TrackerListItem.jsx
+++ src/full-report/components/TrackerListItem.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { ListItem } from 'material-ui/List';
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from 'material-ui/Table';
 
 import NetworkCallConfig from '../../config/network-call.config';
 
@@ -18,6 +26,35 @@ export default class NetworkCalls extends Component {
       return (
         <ListItem
           primaryText={`${trackerData.displayName} (${trackerCalls.length})`}
+          primaryTogglesNestedList
+          nestedItems={[
+            (<div style={{ marginLeft: '10px' }}>
+              <Table>
+                <TableHeader
+                  displaySelectAll={false}
+                  adjustForCheckbox={false}
+                  selectable={false}
+                >
+                  <TableRow>
+                    <TableHeaderColumn style={{ width: '450px' }}>URL</TableHeaderColumn>
+                    <TableHeaderColumn>Method</TableHeaderColumn>
+                    <TableHeaderColumn>Status</TableHeaderColumn>
+                    <TableHeaderColumn>Type</TableHeaderColumn>
+                  </TableRow>
+                </TableHeader>
+                <TableBody displayRowCheckbox={false}>
+                  {trackerCalls.map(tracker => (
+                    <TableRow>
+                      <TableRowColumn style={{ width: '450px' }}>{tracker.data.url}</TableRowColumn>
+                      <TableRowColumn>{tracker.data.method}</TableRowColumn>
+                      <TableRowColumn>{tracker.data.statusCode}</TableRowColumn>
+                      <TableRowColumn>{tracker.data.type}</TableRowColumn>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>),
+          ]}
         />
       );
     }
