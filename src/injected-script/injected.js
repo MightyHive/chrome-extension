1a1cd3dbe3072a2842b5cf57fafc393ed6fa64e0
Reid Greer <jreid.greer@gmail.com>
2017-10-01 13:59:02 -0700

    perf: Checks for Data Layer equality before sending
    Prevents multiple "change" events being sent by the background script by checking if Data Layers
    have been actually updated.

--- src/injected-script/injected.js
+++ src/injected-script/injected.js
@@ -1,56 +1,73 @@
+import equal from 'deep-equal';
 import dataLayerConfig from '../config/data-layers.config';
 
-function getDataLayers() {
-  /**
-   * Iterates through the window object to find matches with
-   * keys passed in via a config.
-   * @param {array} layers - a list of potential data layers and keys
-   */
-  function searchForDataLayers(layers) {
-    const result = [];
-    layers.forEach((layer) => {
-      if (layer.key in window) {
-        result.push({
-          id: layer.id,
-          key: layer.key,
-          displayName: layer.displayName,
-          type: layer.type,
-          data: window[layer.key],
-        });
-      }
-    });
-    return result;
-  }
-
-  let customLayerList = [];
-  // Look for user-defined data layers
-  if (window._customLayerList) {
-    customLayerList = window._customLayerList;
-  }
-
-  const combinedLayerConfig = dataLayerConfig.layers.concat(customLayerList);
-  const loadedDataLayers = searchForDataLayers(combinedLayerConfig);
-
-
-  // Create a custom DOM event to allow passing of data between the
-  // content script and the injected script.
-  const event = new CustomEvent('mh-data-layer-loaded', {
-    detail: JSON.stringify(loadedDataLayers),
-  });
-
-  // Dispatch the event.
-  document.body.dispatchEvent(event);
-}
-
-(() => {
-  let timesToCheck = 40;
-  getDataLayers();
-
-  setTimeout(() => {
-    console.log('timeout');
-    if (timesToCheck > 0) {
-      getDataLayers();
-      timesToCheck -= 1;
+((window) => {
+  function getDataLayers() {
+    /**
+     * Iterates through the window object to find matches with
+     * keys passed in via a config.
+     * @param {array} layers - a list of potential data layers and keys
+     */
+    function searchForDataLayers(layers) {
+      const result = [];
+      layers.forEach((layer) => {
+        if (layer.key in window) {
+          result.push({
+            id: layer.id,
+            key: layer.key,
+            displayName: layer.displayName,
+            type: layer.type,
+            data: window[layer.key],
+          });
+        }
+      });
+      return result;
     }
-  }, 500);
-})();
+
+    let customLayerList = [];
+    // Look for user-defined data layers
+    if (window._customLayerList) {
+      customLayerList = window._customLayerList;
+    }
+
+    const combinedLayerConfig = dataLayerConfig.layers.concat(customLayerList);
+    return searchForDataLayers(combinedLayerConfig);
+  }
+  /**
+   * Sends data layers through a DOM event.
+   * @param {object} data - data layers
+   */
+  function sendDataLayers(data) {
+    // Create a custom DOM event to allow passing of data between the
+    // content script and the injected script.
+    const event = new CustomEvent('mh-data-layer-loaded', {
+      detail: JSON.stringify(data),
+    });
+
+    // Dispatch the event.
+    document.body.dispatchEvent(event);
+  }
+
+  let timesToCheck = 40;
+  let loadedDataLayers = getDataLayers();
+  sendDataLayers(loadedDataLayers);
+
+  // Checks for 20 seconds if there is new Data Layers loaded
+  function checkForDataLayers() {
+    setTimeout(() => {
+      if (timesToCheck > 0) {
+        const tempDataLayers = getDataLayers();
+
+        // Avoid duplicate "change" events triggering on the BG script
+        if (!equal(tempDataLayers, loadedDataLayers)) {
+          loadedDataLayers = tempDataLayers;
+          sendDataLayers(loadedDataLayers);
+        }
+        timesToCheck -= 1;
+        checkForDataLayers();
+      }
+    }, 500);
+  }
+
+  checkForDataLayers();
+})(window);
