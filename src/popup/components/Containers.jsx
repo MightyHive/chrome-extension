dcf07d5cd5b295ab0ea43865619606deaeeae3aa
Reid Greer <jreid.greer@gmail.com>
2017-10-28 17:51:04 -0700

    refactor: Convert to stateless components

--- src/popup/components/Containers.jsx
+++ src/popup/components/Containers.jsx
@@ -1,44 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { List } from 'material-ui/List';
 
 import ContainerItem from './ContainerItem';
 
-class Containers extends Component {
-  static propTypes = {
-    tab: PropTypes.object.isRequired,
-  }
+const Containers = ({ tab }) => {
+  const { containers } = tab;
 
-  render() {
-    const { containers } = this.props.tab;
-
-    if (Object.keys(containers).length > 0) {
-      return (
-        <List>
-          {Object.keys(containers).map((containerId) => {
-            const containerItems = containers[containerId];
-            return (
-              <ContainerItem
-                containerItems={containerItems}
-                containerId={containerId}
-                key={`container-${containerId}`}
-              />
-            );
-          })}
-        </List>
-      );
-    }
-
+  if (Object.keys(containers).length > 0) {
     return (
-      <div
-        style={{
-          textAlign: 'center',
-        }}
-      >
-        <h3 className="no-top-margin">No containers found.</h3>
-      </div>
+      <List>
+        {Object.keys(containers).map((containerId) => {
+          const containerItems = containers[containerId];
+          return (
+            <ContainerItem
+              containerItems={containerItems}
+              containerId={containerId}
+              key={`container-${containerId}`}
+            />
+          );
+        })}
+      </List>
     );
   }
-}
+
+  return (
+    <div
+      style={{
+        textAlign: 'center',
+      }}
+    >
+      <h3 className="no-top-margin">No containers found.</h3>
+    </div>
+  );
+};
+
+Containers.propTypes = {
+  tab: PropTypes.object.isRequired,
+};
 
 export default Containers;
--- src/popup/components/Home.jsx
+++ src/popup/components/Home.jsx
@@ -1,37 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class Home extends Component {
-  static propTypes = {
-    tab: PropTypes.object.isRequired,
-  }
-
-  render() {
-    const { tab } = this.props;
-
-    return (
-      <div className="custom-container">
-        <div className="trackerCount">
-          <div className="content">
-            {tab.trackerCount || 0}
-          </div>
-          <div className="heading">
-            <h4 className="thin">Trackers</h4>
-          </div>
-        </div>
-        <div className="clearfix">
-          <div className="halfColumn subData">
-            <h5>Containers</h5>
-            <span className="data">{Object.keys(tab.containers).length}</span>
-          </div>
-          <div className="halfColumn subData">
-            <h5>Network Calls</h5>
-            <span className="data">{tab.networkCalls.all.length}</span>
-          </div>
-        </div>
+const Home = ({ tab }) => (
+  <div className="custom-container">
+    <div className="trackerCount">
+      <div className="content">
+        {tab.trackerCount || 0}
       </div>
-    );
-  }
-}
+      <div className="heading">
+        <h4 className="thin">Trackers</h4>
+      </div>
+    </div>
+    <div className="clearfix">
+      <div className="halfColumn subData">
+        <h5>Containers</h5>
+        <span className="data">{Object.keys(tab.containers).length}</span>
+      </div>
+      <div className="halfColumn subData">
+        <h5>Network Calls</h5>
+        <span className="data">{tab.networkCalls.all.length}</span>
+      </div>
+    </div>
+  </div>
+);
+
+Home.propTypes = {
+  tab: PropTypes.object.isRequired,
+};
 
 export default Home;
