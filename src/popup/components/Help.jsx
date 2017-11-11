4975088369f90c4b5d1b8871966640ab0d405703
Michael Balarezo <michael.balarezo@mightyhive.com>
2017-11-10 23:26:46 -0800

    Adds three questions and articles to help section

--- src/popup/components/Help.jsx
+++ src/popup/components/Help.jsx
@@ -7,30 +7,23 @@ const Help = () => (
     <List>
       <ListItem
         primaryTogglesNestedList
-        primaryText="Example Text"
+        primaryText="I Don't See My Data Layer. How can I add it?"
         nestedItems={[
-          <span>Example nested text.</span>,
+          <span>If you're using a tag management system other than Google Tag Manager or Tealium, or if you've renamed the data layer, then the extension will not be able to identify them by default. To add a new data layer, click the View Full Report button. Next, click the "DATA LAYERS" tab, and click the link at the bottom of the page. This will open a new window and present you with a dialog box to enter the name of your custom data layer.</span>,
         ]}
       />
       <ListItem
         primaryTogglesNestedList
-        primaryText="Example Text"
+        primaryText="How Can I See More Data about Trackers?"
         nestedItems={[
-          <span>Example nested text.</span>,
+          <span>Everything on Full Report page is clickable, and will expand to show more information. Clicking on a tracker will expand to show all network activity associated with that vendor. Clicking on a specific call expands additional information like the HTTP status code of that request, and any query parameters being passed back to the server.</span>,
         ]}
       />
       <ListItem
         primaryTogglesNestedList
-        primaryText="Example Text"
+        primaryText="How Do I Access Details For The Redirect Path In The Full Report?"
         nestedItems={[
-          <span>Example nested text.</span>,
-        ]}
-      />
-      <ListItem
-        primaryTogglesNestedList
-        primaryText="Example Text"
-        nestedItems={[
-          <span>Example nested text.</span>,
+          <span>The redirect path can be seen by clicking on the View Full Report button. The redirect path is shown at the top of the Full Report page. The circles represent all of the domains that were hit on the way to the current page, and can be hovered over for more information.</span>,
         ]}
       />
     </List>
