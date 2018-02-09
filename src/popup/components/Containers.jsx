c7697b04a9f9e989e7aea2c01ddf5dfff2582c2e
Kevin McGovern <kevin.mcgovern92@gmail.com>
2018-02-08 16:41:19 -0800

    Adds ga event on navigation to Containers tab - potentially done poorly

--- src/popup/components/Containers.jsx
+++ src/popup/components/Containers.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { List } from 'material-ui/List';
+import ReactGA from 'react-ga';
 
 import ContainerItem from './ContainerItem';
 
 const Containers = ({ tab }) => {
   const { containers } = tab;
+  ReactGA.initialize('UA-37980828-6');
 
   if (Object.keys(containers).length > 0) {
     return (
@@ -14,6 +16,11 @@ const Containers = ({ tab }) => {
         <List>
           {Object.keys(containers).map((containerId) => {
             const containerItems = containers[containerId];
+            ReactGA.event({
+              category: 'Pop Up',
+              action: 'View Containers Tab',
+              nonInteraction: true,
+            });
             return (
               <ContainerItem
                 containerItems={containerItems}
